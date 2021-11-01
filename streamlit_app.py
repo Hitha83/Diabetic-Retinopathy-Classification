0d15595d5c012c664c4d64275082857239ce26fb
Hitha MV <65976178+Hitha83@users.noreply.github.com>
2021-11-01 19:43:33 +0530

    no message

---
+++ streamlit_app.py
@@ -0,0 +1,33 @@
+import streamlit as st
+import as tensorflow as tf
+st.set_option("deprecation.showfileUploaderEncoding",False)
+@st.cache(allow_output_mutation=True)
+def load_model():
+  model = tf.keras.models.load_model('/content/gdrive/MyDrive/DR_model/best_model')
+  return model
+model = load_model()
+st.write("""
+          # Diabetic Retinopathy Classification
+          """)
+
+file = st.file_uploader("Please upload your image dataset", type = ["jpg", "png", "jpeg"])
+import cv2
+from PIL import Image, ImageOps
+import numpy as np
+def import_and_predict(image_data, model):
+  size = (224,224)
+  image = ImageOps.fit(image_data, size, Image.ANTIALIAS)
+  img = np.asarray(image)
+  img_reshape = img[np.newaxis,...]
+  img_proc = preprocess_input(Xtrain)
+  prediction = model.predict(img_proc)
+  return prediction
+if file is None:
+  st.text("Please upload an image file")
+else:
+  image = Image.open(file)
+  st.image(image,use_column_width=True)
+  predictions = import_and_predict(image,model)
+  class_names = ["No DR","Mild","Moderate","Severe","Proliferative DR"]
+  string = "This image most likely is: "+class_names[np.argmax(predictions)]
+  st.success(string)
