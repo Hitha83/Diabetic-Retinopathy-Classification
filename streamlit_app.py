bd7b6f3d8b1cb532158759ece5c69b3a4e4905ff
Hitha MV <65976178+Hitha83@users.noreply.github.com>
2021-11-22 23:42:33 +0530

    no message

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@
 
 import streamlit as st
 import tensorflow as tf
+import tensorflow_hub as hub
 # Packages required for Image Classification
 from keras.preprocessing.image import load_img
 from keras.preprocessing.image import img_to_array
@@ -23,35 +24,39 @@ import matplotlib.image as mpimg
 fig = plt.figure()
 
 with open("custom.css") as f:
-  st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+    st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
 st.header("Diabetic Retinopathy Grade Classifier")
 
 st.markdown("A simple web application for grading severity of diabetic retinopathy . The presence of Diabetic retinopathy are classified into five different grades namely: 0 - No DR, 1 - Mild, 2 - Moderate, 3 - Severe, 4 - Proliferative DR.")
 
 def main():
-  file_uploaded = st.file_uploader("Please upload your image dataset", type = ["jpg", "png", "jpeg"])
-  class_btn = st.button("Classify")
-  if file_uploaded is not None:
-    image = Image.open(file_uploaded)
-    st.image(image, caption='Uploaded Image', use_column_width=True)
-    with open(os.path.join(".",file_uploaded.name),"wb")as f:
-      f.write(file_uploaded.getbuffer())
-    st.success("File saved")
+    file_uploaded = st.file_uploader("Please upload your image dataset", type = ["jpg", "png", "jpeg"])
+    class_btn = st.button("Classify")
+    if file_uploaded is not None:
+        image = Image.open(file_uploaded)
+        st.image(image, caption='Uploaded Image', use_column_width=True)
+        with open(os.path.join(".",file_uploaded.name),"wb")as f:
+            f.write(file_uploaded.getbuffer())
+        st.success("File saved")
         
-  if class_btn:
-    if file_uploaded is None:
-      st.write("Invalid command, please upload an image")
-    else:
-      with st.spinner('Model working....'):
-        plt.imshow(image)
-        plt.axis("off")
-        label = import_and_predict(image)
-        st.write('%s (%.2f%%)' % (label[1], label[2]*100))
-        st.success('Classified')
-        st.write(label)
+    if class_btn:
+        if file_uploaded is None:
+            st.write("Invalid command, please upload an image")
+        else:
+            with st.spinner('Model working....'):
+                plt.imshow(image)
+                plt.axis("off")
+                label = import_and_predict(image)
+                st.write('%s (%.2f%%)' % (label[1], label[2]*100))
+                st.success('Classified')
+                st.write(label)
         
-              
+                
+                #scores = tf.nn.softmax(predictions[0])
+                #scores = scores.numpy()
+                #data = predict_on_image_set(scores)        
+                #st.dataframe(data)
                 
 def crop_image_from_gray(image,tol=7):
     if image.ndim ==2:
@@ -73,23 +78,21 @@ def crop_image_from_gray(image,tol=7):
     #print(image.shape)
         return image
                 
-               
                 
             
 def import_and_predict(image):
-
-  model = classifier_model = tf.keras.models.load_model('DR3000-60.h5')
-  
-  image = image.resize(128, 128)
-  image=cv2.GaussianBlur( image , (5,5) ,0)
-  image = np.array(image, dtype="float") / 255.0
-  image = image.reshape((1, image.shape[0], image.shape[1], image.shape[2]))
-  yhat = model.predict(image)
-  # convert the probabilities to class labels
-  label = decode_predictions(yhat)
-  # retrieve the most likely result, e.g. highest probability
-  label = label[0][0]
-  return label 
+    model = classifier_model = tf.keras.models.load_model(r'/content/gdrive/MyDrive/DR_MODEL/DR3000-60.h5')
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    image = cv2.resize(image, (128, 128))
+    image=cv2.GaussianBlur( image , (5,5) ,0)
+    image = np.array(image, dtype="float") / 255.0
+    image = image.reshape((1, image.shape[0], image.shape[1], image.shape[2]))
+    yhat = model.predict(image)
+    # convert the probabilities to class labels
+    label = decode_predictions(yhat)
+    # retrieve the most likely result, e.g. highest probability
+    label = label[0][0]
+    return label 
 
 
 if __name__ == '__main__':
