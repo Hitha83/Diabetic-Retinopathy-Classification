a8284b04f54aecc844011100be8663f6319ecf5e
Hitha MV <65976178+Hitha83@users.noreply.github.com>
2021-11-22 21:42:54 +0530

    no message

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,10 +39,10 @@ def main():
                 plt.imshow(image2)
                 plt.axis("off")
                 
-		class_names = {0: "No DR", 1: "Mild", 2: "Moderate", 3: "Severe", 4: "Proliferative DR"}
+		#class_names = {0: "No DR", 1: "Mild", 2: "Moderate", 3: "Severe", 4: "Proliferative DR"}
 		result = import_and_predict(image)
 		
-                string  = "This image belongs to "+ class_names[np.argmax(result)]
+                string  = "This image belongs to "+ np.argmax(result)
                 st.success('Classified')
                 st.write(string)
 	    
