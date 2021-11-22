2314420fa173ad9dc4520790db771cba97ee7d96
Hitha MV <65976178+Hitha83@users.noreply.github.com>
2021-11-22 18:07:18 +0530

    no message

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import streamlit as st
 import tensorflow as tf
 from keras.applications.vgg16 import VGG16
 from PIL import Image
-import cv2
+from .cv2 import *
 import numpy as np
 import matplotlib.pyplot as plt
 from tensorflow.keras.models import load_model
