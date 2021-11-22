4415be432e14dfaf6358161fa7bd4ac5939dd826
Hitha MV <65976178+Hitha83@users.noreply.github.com>
2021-11-22 19:51:45 +0530

    no message

--- streamlit_app.py
+++ streamlit_app.py
@@ -72,8 +72,8 @@ def crop_image_from_gray(image,tol=7):
             
 def import_and_predict(image):
     model = classifier_model = tf.keras.models.load_model('DR3000-60.h5')
-    #image = cv2.imread('file_uploaded')
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    image = cv2.imread('file_uploaded')
+    #image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     image = crop_image_from_gray(image)
     image = cv2.resize(image, (128, 128))
     image=cv2.GaussianBlur( image , (5,5) ,0)
