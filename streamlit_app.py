ae6c3d5658e0b8d2c048d4602c50378225fd5170
Hitha MV <65976178+Hitha83@users.noreply.github.com>
2021-11-24 21:15:52 +0530

    no message

--- streamlit_app.py
+++ streamlit_app.py
@@ -78,8 +78,8 @@ def main():
                    st.session_state.a = a
                 else:
                    session_df = pd.DataFrame(st.session_state.a)
-                   #final_df = session_df.append(new_row, ignore_index=True)
-                   #st.session_state.a= final_df        
+                   final_df = session_df.append(new_row, ignore_index=True)
+                   st.session_state.a= final_df        
                    st.write(session_df)
   
                      
