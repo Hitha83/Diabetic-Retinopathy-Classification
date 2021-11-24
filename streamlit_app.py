555aad1d8008755ffbd812ae1163e30f2ba90a55
Hitha MV <65976178+Hitha83@users.noreply.github.com>
2021-11-24 13:07:25 +0530

    no message

--- streamlit_app.py
+++ streamlit_app.py
@@ -75,7 +75,7 @@ def main():
                    st.session_state['key'] = df
                 else:
                    session_df = st.session_state['key']
-                   final_df = session_df.append(new_row)
+                   final_df = session_df.append(new_row, ignore_index=True)
                    st.session_state['key'] = final_df
                    
                 #d = {'image': [], 'results': []}
