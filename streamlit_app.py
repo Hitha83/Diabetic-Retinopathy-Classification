313123e2ebf656eadb71da91f47f429bc8503683
Hitha MV <65976178+Hitha83@users.noreply.github.com>
2021-11-24 11:09:07 +0530

    no message

--- streamlit_app.py
+++ streamlit_app.py
@@ -65,12 +65,14 @@ def main():
                    st.session_state['key'] = {'image': file_uploaded.name, 'results':scores, 'maxScore':scoreArr}
                 else:
                    data_dict= st.session_state['key']
-                   final_dict = data_dict.copy()
+                   data_dict["image"].append(file_uploaded.name)
+                   #final_dict = data_dict.copy()
                    #newfile="dfdf"
                    #final_dict["image"].append(newfile)
                    data_dict["results"].append(scores)
                    data_dict["maxScore"].append(scoreArr)
-                   st.session_state['key'] = final_dict
+                   st.session_state['key'] = data_dict
+                   st.write(data_dict)
                 #d = {'image': [], 'results': []}
 
                 data = []
