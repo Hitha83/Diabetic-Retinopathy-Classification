2cd66aa809dd76341feb5f3b3730ed008eca843e
Hitha MV <65976178+Hitha83@users.noreply.github.com>
2021-11-25 09:59:45 +0530

    no message

--- streamlit_app.py
+++ streamlit_app.py
@@ -61,17 +61,15 @@ def main():
                 new_row = {'image': image_names, 'results': scores, 'maxScore': scoreArr}
                 st.success('Classified')
                 st.write(result)
-                new_row = {'image': image_names, 'results': scores, 'maxScore': scoreArr}
                 a = pd.DataFrame(new_row)
 
                 if 'a' not in st.session_state:
-                    st.session_state.a = df
                     st.session_state.a = a
                 else:
                     session_df = pd.DataFrame(st.session_state.a)
                     st.write(session_df)
-                    # final_df = session_df.append(new_row, ignore_index=True)
-                    # st.session_state.a = final_df
+                    final_df = session_df.append(new_row, ignore_index=True)
+                    st.session_state.a = final_df
 
 
 def import_and_predict(image):
