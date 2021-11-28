7e21cc5fa964bb908f6f231f78b63a67a41d18c6
Hitha MV <65976178+Hitha83@users.noreply.github.com>
2021-11-28 21:34:28 +0530

    no message

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ from keras.applications.vgg16 import preprocess_input
 from keras.applications.vgg16 import decode_predictions
 from keras.applications.vgg16 import VGG16
 from PIL import Image
-import cv2
+#import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 from tensorflow.keras.models import load_model
