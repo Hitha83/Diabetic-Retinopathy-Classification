3346738d1aa5e7fb27cc4b4928fd4e3691a296ee
Hitha MV <65976178+Hitha83@users.noreply.github.com>
2021-11-28 22:20:56 +0530

    no message

--- streamlit_app.py
+++ streamlit_app.py
@@ -102,10 +102,10 @@ def main():
 
 
 
-def import_and_predict(image):
+def import_and_predict(file_uploaded):
     model = classifier_model = tf.keras.models.load_model('DR3000-60.h5')
     image = cv2.imread('file_uploaded')
-    image = cv2.resize(image,(128,128))
+    image = cv2.resize(image,(128,128), interpolation=cv2.INTER_AREA)
     image = cv2.GaussianBlur( image, (5,5),0)
     image = np.array(image, dtype="float") / 255.0
     image = image.reshape((1, image.shape[0], image.shape[1], image.shape[2]))
