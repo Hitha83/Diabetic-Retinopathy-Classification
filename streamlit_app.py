0bc4336b426e0ee01cb9267fb5da95f16dada8cf
Hitha MV <65976178+Hitha83@users.noreply.github.com>
2021-11-29 12:06:18 +0530

    no message

--- streamlit_app.py
+++ streamlit_app.py
@@ -112,10 +112,10 @@ def main():
                     # if dataframe_btn:
 
                     # image sidebar
-    final_data = pd.DataFrame(st.session_state.a)
+        final_data = pd.DataFrame(st.session_state.a)
 
-    images = final_data['image']
-    st.write(images)
+        images = final_data['image']
+        st.write(images)
     classes = final_data['classes']
     image_choice = st.sidebar.selectbox('Select image:', images)
     st.write(image_choice in images)
