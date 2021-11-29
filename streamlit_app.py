e8e8ac28b91bec50d206161655b5b40f03b6cd1c
Hitha MV <65976178+Hitha83@users.noreply.github.com>
2021-11-29 08:30:12 +0530

    no message

--- streamlit_app.py
+++ streamlit_app.py
@@ -57,7 +57,7 @@ def main():
                 prob = import_and_predict(image)
                 #prob_scores.append(prob[np.argmax(prob)])
                 class_value = np.argmax(prob,axis =1)
-                classes.append(class_value[0])
+                classes.append(class_value)
 
                 new_row = {'image': image_names,'classes': classes}
                 st.success('Classified')
