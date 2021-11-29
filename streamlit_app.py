53a166909c6be5e2caa4371dd6c1344792d407c2
Hitha MV <65976178+Hitha83@users.noreply.github.com>
2021-11-29 17:26:39 +0530

    no message

--- streamlit_app.py
+++ streamlit_app.py
@@ -57,11 +57,11 @@ def main():
                 prob = import_and_predict(image)
                 #prob_scores.append(prob[np.argmax(prob)])
                 class_value = np.argmax(prob,axis =1)
-                classes.append(class_value[0])
+                classes.append(class_value)
                 st.success('Classified')
                 st.write("Diabetic retinopathy image grade is: "+str(class_value[0]))
 
-                new_row = {'image': image_names[0],'classes': classes[0]}
+                new_row = {'image': image_names[0],'classes': classes[0][0]}
 
 
                 a = pd.DataFrame(new_row)
