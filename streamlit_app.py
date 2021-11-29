563f06dea11d5f093f516d277d1307092a6f0d5f
Hitha MV <65976178+Hitha83@users.noreply.github.com>
2021-11-29 08:39:03 +0530

    no message

--- streamlit_app.py
+++ streamlit_app.py
@@ -87,7 +87,7 @@ def main():
 
 
                 st.write('Line_chart.')
-                st.line_chart(final_data['classes'])
+                st.line_chart(final_data)
                 st.write('classes')
                 #st.bar_chart(final_df['classes'])
                 #final_df['classes'].hist(figsize=(10, 5))
