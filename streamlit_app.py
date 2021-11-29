10fc3f453990bcde996c3930053cc6e64b76a8f8
Hitha MV <65976178+Hitha83@users.noreply.github.com>
2021-11-29 13:06:14 +0530

    no message

--- streamlit_app.py
+++ streamlit_app.py
@@ -84,7 +84,7 @@ def main():
         st.markdown(download_csv('predicted Data Frame', final_data), unsafe_allow_html=True)
         images = final_data['image']
         classes = final_data['classes']
-        st.write(classes)
+        st.write(images.values)
         st.write(classes.dtype)
         image_choice = st.sidebar.selectbox('Select image:', images)
         st.write(image_choice in images.values)
