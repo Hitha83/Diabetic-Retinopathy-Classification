d8043f6c0e45d633e3875294abb82f5a2378c19f
Hitha MV <65976178+Hitha83@users.noreply.github.com>
2021-11-30 18:36:56 +0530

    no message

--- streamlit_app.py
+++ streamlit_app.py
@@ -87,8 +87,8 @@ def main():
         st.markdown(download_csv('predicted Data Frame', final_data), unsafe_allow_html=True)
 
         # get count of each type
-        class_count = pd.DataFrame(final_data['classes'].value_counts()).rename(columns={'classes': 'Num_Values'},)
-        class_count.reset_index(inplace=True)
+        class_count = pd.DataFrame(final_data['classes'].value_counts()).reset_index(inplace=True).rename(columns={'classes': 'Num_Values'},)
+        class#count.reset_index(inplace=True)
         #class_count.set_index('index', drop=False, inplace=True)
         st.write(class_count)
         plt.figure(figsize=(15, 6))
