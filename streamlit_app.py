3dc0c72857b88ea9cc70511577188eec4b479fe1
Hitha MV <65976178+Hitha83@users.noreply.github.com>
2021-11-30 21:01:05 +0530

    no message

--- streamlit_app.py
+++ streamlit_app.py
@@ -83,33 +83,14 @@ def main():
             st.title('Final DataFrame')
             st.write(final_data)
             st.markdown(download_csv('predicted Data Frame', final_data), unsafe_allow_html=True)
-            # get count of each type
-            class_count = pd.DataFrame(final_data['classes'].value_counts()).rename(columns={'classes': 'Num_Values'})
-            class_count = class_count.rename_axis('class').reset_index()
-            st.write(class_count)
+            images = final_data['image']
+            classes = final_data['classes']
+            image_choice = st.sidebar.selectbox('Select image:', images)
 
-            #(chart + chart_rule).interactive()
-            bar = alt.Chart(class_count).mark_bar(opacity=0.7).encode(x = 'Num_Values',y ='class')
-            #color = alt.Color('color:N', scale=None)
-            text = bar.mark_text(align='left',baseline='middle',dx=3).encode(text='Num_values')
-            # Nudges text to right so it doesn't appear on top of the bar
-            (bar + text).properties(height=900)
-            #   st.altair_chart(chart, use_container_width=True)
-            #st.title('Diabetic Retinopathy Class Distribution')
-
-        images = final_data['image']
-        classes = final_data['classes']
-        image_choice = st.sidebar.selectbox('Select image:', images)
-
-        if image_choice in images.values:
-            st.write(image_choice)
-            img_class = final_data["classes"].loc[final_data["image"] == image_choice]
-            st.write(img_class)
-
-                    # final_data['prob>80%'] = final_data[final_data['probability']>8.0]
-                    # st.subheader("Image Disease Grades with probability more than 80%")
-                    # if st.checkbox("Show Data"):
-                    #    st.subheader("Data")
+            if image_choice in images.values:
+                st.write(image_choice)
+                img_class = final_data["classes"].loc[final_data["image"] == image_choice]
+                st.write(img_class)
 
 def import_and_predict(image):
     model = classifier_model = tf.keras.models.load_model('DR3000-60.h5')
